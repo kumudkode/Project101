c3e2b2a1487afb4dd37584891835bb73d79b640f
KumudRanjan <raj94971@gmail.com>
2025-10-16 00:29:34 +0530

    task completed

---
+++ Web-project/Task2/cal.js
@@ -0,0 +1,32 @@
+let num1= 12
+let num2= 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+let sumEl = document.getElementById("sum-el")
+let subEl = document.getElementById("sub-el")
+let divEl = document.getElementById("div-el")
+let mulEl = document.getElementById("mul-el")
+
+function add() {
+    sumEl.textContent = "Sum: "+ num1 + num2
+}
+function sub(){
+    subEl.textContent = "Sub: "+ (num1 - num2)
+    // subEl.textContent = "Sub: "+ num1 - num2 // NAN 
+}
+function div(){
+    divEl.textContent = (num1 / num2) // 3.2857142857142856
+    divEl.textContent = "Div: "+Math.floor(num1 / num2) // 3
+    // using math.floor we can round off the value
+}
+function mul(){
+    mulEl.textContent = "Mul: "+num1 * num2
+}
---
+++ Web-project/Task3/script.js
@@ -0,0 +1,28 @@
+let value = 10;
+document.getElementById("cont").textContent = value;
+let value2 = 20;
+document.getElementById("cont2").textContent = value2;
+function add(){
+    value = value + 1;
+    document.getElementById("cont").textContent = value;
+}
+function add2(){
+    value = value + 2;
+    document.getElementById("cont").textContent = value;
+}
+function add3(){
+    value = value + 3;
+    document.getElementById("cont").textContent = value;
+}
+function sum(){
+    value = value + 1;
+    document.getElementById("cont2").textContent = value;
+}
+function sum2(){
+    value = value + 2;
+    document.getElementById("cont2").textContent = value;
+}
+function sum3(){
+    value = value + 3;
+    document.getElementById("cont").textContent = value;
+}
