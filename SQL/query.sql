62021df7c84e1acfe1e176cb1013b1a41867cc7f
KumudRanjan <raj94971@gmail.com>
2025-10-26 16:26:52 +0530

    modifyed sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -93,7 +93,8 @@
 		where the price is between $20,000 and $60,000
 		and the condition is between 1 and 3
 		and the color contains red
-*/ SELECT brand, model, condition, color, price FROM cars
+*/ 
+SELECT brand, model, condition, color, price FROM cars
 WHERE price BETWEEN 20000 AND 60000
 AND condition BETWEEN 1 AND 3
 AND color Like '%red%';
