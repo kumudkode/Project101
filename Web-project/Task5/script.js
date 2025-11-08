88b4d98de22297707f044165dcff3b0c0b8dbc99
KumudRanjan <raj94971@gmail.com>
2025-11-08 14:02:28 +0530

    Updated

---
+++ Web-project/Task5/script.js
@@ -0,0 +1,11 @@
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function() {
+   console.log("Button clicked");
+  
+})
+let box = document.getElementById("box");
+
+box.addEventListener("click", function() {
+    box.style.backgroundColor = "red";
+})
