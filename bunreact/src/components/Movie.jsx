473074ba214c120da2601b1ff1a7cb893de0a3e7
KumudRanjan <raj94971@gmail.com>
2025-11-12 17:52:29 +0530

    updated css still have many issue in css

--- bunreact/src/components/Movie.jsx
+++ bunreact/src/components/Movie.jsx
@@ -1,16 +1,48 @@
-import './Movie.css'
-function Movie({title}){
-    return(
-   <>
-        <div className="root">
-            <img src="image.png" alt="movie png" />
-            <h3> {title}</h3>
-            <h4> Movie cast</h4>
-            <button>Watchlist</button>
-            <button> Trailer </button>
-    
-        </div>
-   </>
-    )
+import "./Movie.css";
+function Movie({ title,star }) {
+    return (
+
+        <>
+            <div>
+                <h1> Fan Favorites</h1>
+                <p> This week's top TV and Movies </p>
+            </div>
+            <div className="root">
+                <div className="star">
+                 {star}
+                </div>
+                <div className="add"> 
+                    
+                 </div>
+                <div className="Scroller">
+                <img src="image.png" alt="movie png" />
+                <h3> {title}</h3>
+                <p> Movie cast</p>
+                <button>
+                    <svg
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                    >
+                        <path d="M3 12h18M3 6h18M3 18h18" />
+                    </svg>
+                    Watchlist
+                </button>
+
+                <button>
+                    <svg
+                        viewBox="0 0 24 24" 
+                        fill="currentColor"
+                    >
+                        <path d="M8 5v14l11-7z" />
+                    </svg>
+                    Trailer
+                </button>
+                </div>
+            </div>
+           
+        </>
+    );
 }
-export default Movie
+export default Movie;
--- bunreact/src/components/MovieTab.jsx
+++ bunreact/src/components/MovieTab.jsx
@@ -5,12 +5,12 @@ function MovieTab(){
     return(
         
         <div className="tab">
-        <Movie title="Kantara A Legend: Chapter 1" />
-        <Movie title="Lokah Chapter One: Chandra"/>
-        <Movie title="The Great Pre-Wedding Show" />
-        <Movie title="Frankenstein"/>
-        <Movie title="Predator: Badlands" />
-        <Movie title="The Fantastic Four: First Steps" />
+        <Movie title="Kantara A Legend: Chapter 1" star="8.4"/>
+        <Movie title="Lokah Chapter One: Chandra" star="7.8" />
+        <Movie title="The Great Pre-Wedding Show" star="8.9" />
+        <Movie title="Frankenstein" star="7.6"/>
+        <Movie title="Predator: Badlands" star="7.6"/>
+        <Movie title="The Fantastic Four: First Steps" star="7.0" />
         
        </div>
 
