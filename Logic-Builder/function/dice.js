21ab5f64edd7644a2f2702519c923c06c25debb9
KumudRanjan <raj94971@gmail.com>
2025-11-19 20:54:58 +0530

    dice.js

---
+++ Logic-Builder/function/dice.js
@@ -0,0 +1,10 @@
+// create a function to roll a dice and always display random values(1-6)
+function roll(){
+    let random = Math.floor(Math.random() * 6)+ 1
+    console.log(random)
+}
+roll()
+
+// for(let i=0; i<30; i++){
+//     roll();
+// }
