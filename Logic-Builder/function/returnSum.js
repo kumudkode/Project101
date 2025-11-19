8b5796804588abebf37c7f466046dab6714213d2
KumudRanjan <raj94971@gmail.com>
2025-11-19 21:20:49 +0530

    returnSum.js

---
+++ Logic-Builder/function/returnSum.js
@@ -0,0 +1,16 @@
+// create a function that returns the sum of number from 1 to n
+function sum(n){
+    return n*(n+1)/2
+}
+console.log(sum(3)) 
+
+// or 
+function getSum(m){
+let sum = 0;
+for(let i = 0;i<=m;i++){
+    sum += i;
+    console.log(i +" "+m+" = "+ sum)
+}
+return sum
+}
+console.log(getSum(3))
