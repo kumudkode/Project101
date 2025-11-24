44064e714958a39f0d6acdbd6e86c4c5735f7e02
KumudRanjan <raj94971@gmail.com>
2025-11-25 00:00:42 +0530

    methods

--- Logic-Builder/function/methods.js
+++ Logic-Builder/function/methods.js
@@ -1,1 +1,20 @@
 //Action that can be performed on an object is called methods
+
+// just like object have variables --> key : value pairs
+const calculator = {
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+       return a-b;
+    },
+    mul: function(a,b){
+       return a*b;
+    },
+    div: function(a,b){
+        return a/b
+    }
+}
+
+//for accessing these value use 
+// calculator.add(2,4) or use any function wth any arguments
