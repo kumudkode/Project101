8af1036f58b4b3c9cab8ef9e499915049b704bf4
KumudRanjan <raj94971@gmail.com>
2025-11-27 23:23:51 +0530

    Template_Literals

---
+++ Logic-Builder/function/Template_Literals.js
@@ -0,0 +1,8 @@
+// when you have to write variables inside js code then 
+let a =23
+let b = 34
+
+console.log("hey this is the way to write string as a number "+(a+b))
+// or you can write like this
+console.log(`hey this is the way to write string as a number ${a+b} `)
+
